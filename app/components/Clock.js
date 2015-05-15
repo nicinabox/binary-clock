d3f7f75291b46abc5626207b8324101013da698e
Nic Aitch <nic@nicinabox.com>
2015-05-15 13:45:48 -0500

    Add dynamic scaling for clock

--- app/components/Clock.js
+++ app/components/Clock.js
@@ -10,7 +10,16 @@ var Clock = React.createClass({
       currentTime: currentTime,
       timeOfDay: time.timeOfDay(),
       parts: time.timeParts(currentTime),
+      dotSize: this._calcDotSize(),
     };
+  },
+
+  componentWillMount() {
+    window.onresize = () => {
+      this.setState({
+        dotSize: this._calcDotSize()
+      });
+    }
   },
 
   componentDidMount() {
@@ -19,6 +28,7 @@ var Clock = React.createClass({
 
       this.setState({
         currentTime: currentTime,
+        timeOfDay: time.timeOfDay(),
         parts: time.timeParts(currentTime),
       });
     }, 1000);
@@ -28,14 +38,31 @@ var Clock = React.createClass({
     clearInterval(this._timer);
   },
 
+  _calcDotSize() {
+    var windowHeight = window.innerHeight;
+    var windowWidth = window.innerWidth;
+
+    var ratio = windowHeight / windowWidth,
+        maxWidth = windowWidth / 6,
+        maxHeight = windowHeight / 4,
+        SCALE = 0.8;
+
+    if (maxWidth * 4 > windowHeight) {
+      maxHeight = windowHeight / 4;
+      return maxHeight * SCALE;
+    } else {
+      return maxWidth * SCALE;
+    }
+  },
+
   _renderTimeDigit(segment, i) {
     return (
       <li key={i}>
         {segment.map((number, i) => {
           return (
-            <span key={i} className={+number ? 'active' : ''}>
-              {number}
-            </span>
+            <span key={i}
+              style={styles.dot(this.state.dotSize)}
+              className={+number ? 'active' : ''}></span>
           );
         })}
       </li>
@@ -53,8 +80,10 @@ var Clock = React.createClass({
   },
 
   render() {
+    var classNames = ['clock', this.state.timeOfDay].join(' ');
+
     return (
-      <div className="clock">
+      <div ref="clock" className={classNames}>
         <div className="clock-container">
           {this.state.parts.map(this._renderTimeSegment)}
         </div>
@@ -65,7 +94,17 @@ var Clock = React.createClass({
       </div>
     );
   }
-
 });
+
+var styles = {
+  dot: (size) => {
+    return {
+      width: size,
+      height: size,
+      marginTop: size * 0.1,
+      marginBottom: size * 0.1,
+    }
+  }
+};
 
 module.exports = Clock;
